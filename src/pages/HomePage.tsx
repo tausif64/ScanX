fc4c06cb98a8ba7c8e44459ecce4803bf555a18c
tausif64 <developer.642003@gmail.com>
2025-01-16 15:43:31 +0530

    Home

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,10 +2,15 @@ import React, { useContext } from 'react';
 import { ThemedView } from '../components/ThemedView';
 import { ThemedText } from '../components/ThemedText';
 import Card from '../components/Card';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 import { SQLiteContext } from '../context/AppContext';
+import { NavigationProp } from '@react-navigation/native';
 
-const HomePage = () => {
+interface Props {
+  navigation: NavigationProp<any>;
+}
+
+const HomePage = ({ navigation }: Props) => {
   const context = useContext(SQLiteContext);
 
   if (!context) {
@@ -16,8 +21,17 @@ const HomePage = () => {
   return (
     <ThemedView style={styles.container}>
       <ScrollView style={styles.container2}>
-      <ThemedText style={styles.header}>Recently Viewed</ThemedText>
-      {documents.map((item)=><Card key={item.id} document={item} />)}
+        <ThemedText style={styles.header}>Recently Viewed</ThemedText>
+        {documents.map((item) => <TouchableOpacity onPress={() => navigation.navigate('m',
+          {
+            screen: 'Details',
+            params: { id: item?.id },
+          }
+        )}
+          key={item?.id}>
+          <Card document={item} />
+        </TouchableOpacity>
+        )}
 
       </ScrollView>
     </ThemedView>
@@ -28,9 +42,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  container2:{
-    paddingHorizontal:10,
-    paddingVertical:5,
+  container2: {
+    paddingHorizontal: 10,
+    paddingVertical: 5,
   },
   header: {
     fontSize: 20,
