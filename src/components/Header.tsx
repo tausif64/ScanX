02a4bf794f4a0c275df5f868c69a336089e740c9
tausif64 <developer.642003@gmail.com>
2025-02-26 19:40:28 +0530

    refining header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,66 @@
-import React from 'react';
-import { TextInput, StyleSheet, TouchableOpacity } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+/* eslint-disable react-native/no-inline-styles */
+import React, { useContext, useState } from 'react';
+import { StyleSheet, TouchableOpacity, TouchableWithoutFeedback, View, Dimensions, useColorScheme, Text } from 'react-native';
+// import Icon from 'react-native-vector-icons/MaterialIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { ThemedView } from './ThemedView';
+import { ThemedText } from './ThemedText';
+import { SQLiteContext } from '../context/AppContext';
+import { useNavigation, NavigationProp } from '@react-navigation/native';
+import { RootStackParamList } from '../navigation/navigation';
 
-interface HeaderProps {
-    onMenuPress?: () => void;
-    onThreeDotPress?: () => void;
-    onChangeText?: (text: string) => void;
-}
 
-const Header: React.FC<HeaderProps> = ({ onMenuPress, onThreeDotPress, onChangeText }) => {
+const HEIGHT = Dimensions.get('screen').height;
+const WIDTH = Dimensions.get('screen').width;
+
+const Header = () => {
+    const [menuVisible, setMenuVisible] = useState<boolean>(false);
+    const theme = useColorScheme();
+    const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+
+
+    const context = useContext(SQLiteContext);
+
+      const handelPress = (option:string) => {
+          context!.setSortOption(option);
+          setMenuVisible(false);
+      };
+
     return (
         <>
-        <ThemedView style={styles.container}>
-            {/* Left: Menu Icon */}
-            <TouchableOpacity onPress={onMenuPress} style={styles.iconContainer}>
-                <Icon name="menu" size={24} color="#000" />
-            </TouchableOpacity>
+            <ThemedView style={styles.container}>
+                <Text style={styles.icon}>ScanX</Text>
 
-            {/* Middle: TextInput */}
-            <TextInput
-                style={styles.textInput}
-                placeholder={'Search...'}
-                placeholderTextColor="#000"
-                onChangeText={onChangeText}
-            />
 
-            {/* Right: Three-Dot Icon */}
-            <TouchableOpacity onPress={onThreeDotPress} style={styles.iconContainer}>
-                <Icon name="more-vert" size={24} color="#000" />
-            </TouchableOpacity>
-        </ThemedView>
+                <TouchableOpacity onPress={() => navigation.navigate('Search')} style={styles.iconContainer}>
+                    <Ionicons name="search" size={24} color="#000" />
+                </TouchableOpacity>
+                {/* <TouchableOpacity style={styles.iconContainer}>
+                    <Icon name="more-vert" size={24} color="#000" />
+                </TouchableOpacity> */}
+            </ThemedView>
+            {menuVisible && (
+                <>
+                    <TouchableWithoutFeedback onPress={() => setMenuVisible(false)}>
+                            <View style={styles.modalOverlay} />
+                        </TouchableWithoutFeedback>
+                    <ThemedView style={styles.modalContainer}>
+                        <ThemedText style={[styles.modalMenu, {borderColor: theme === 'dark' ? '#fff' : '#000'}]}>Sort</ThemedText>
+                        <TouchableOpacity style={styles.pt} onPress={() => handelPress('name_asc')}>
+                            <ThemedText>Name (A-Z)</ThemedText>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.sortMenu} onPress={() => handelPress('name_desc')}>
+                            <ThemedText>Name (Z-A)</ThemedText>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.sortMenu} onPress={() => handelPress('date_newest')}>
+                            <ThemedText>Date (Newest - Oldest)</ThemedText>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.sortMenu} onPress={() => handelPress('date_oldest')}>
+                            <ThemedText>Date (Oldest - Newest)</ThemedText>
+                        </TouchableOpacity>
+                    </ThemedView>
+                </>
+            )}
         </>
     );
 };
@@ -43,21 +73,41 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         backgroundColor: '#ffffff',
         paddingHorizontal: 16,
-        borderBottomWidth: 1,
-        borderBottomColor: '#e0e0e0',
     },
     iconContainer: {
         padding: 8,
     },
-    textInput: {
-        flex: 1,
-        height: 40,
-        marginHorizontal: 16,
-        paddingHorizontal: 10,
-        backgroundColor: '#f0f0f0',
+    icon: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    modalContainer: {
+        position: 'absolute',
+        top: 20,
+        right: 20,
+        width: 220,
+        paddingVertical: 15,
+        paddingHorizontal: 19,
         borderRadius: 8,
-        fontSize: 16,
-        color: '#000000',
+    },
+    modalOverlay: {
+        flex: 1,
+        position: 'absolute',
+        top:0,
+        left:0,
+        height: HEIGHT,
+        width: WIDTH,
+    },
+    modalMenu: {
+        borderBottomWidth: 1,
+        paddingBottom: 5,
+    },
+    pt: {
+        paddingTop: 12,
+        paddingBlock: 5,
+    },
+    sortMenu: {
+        paddingVertical: 5,
     },
 });
 
