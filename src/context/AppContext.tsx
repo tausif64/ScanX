245ca86c4fe776a1b87d6b8ae68300278f0f58f8
tausif64 <developer.642003@gmail.com>
2025-03-08 08:37:47 +0530

    Implimenting essential functions

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-shadow */
 import React, { createContext, useState, useEffect } from 'react';
-import { db, insertDocument, insertFolder, insertImage, reOrderDocumnetImages, updateDocument } from '../db/db'; // Import your SQLite database functions
+import { db, deleteDocument, deleteFolder, deleteImage, insertDocument, insertFolder, insertImage, reOrderDocumnetImages, updateDocument } from '../db/db';
 import { Document, Folder, ImageProps } from '../interface';
 import DocumentScanner from 'react-native-document-scanner-plugin';
 import compressor from 'react-native-compressor';
@@ -24,13 +24,18 @@ interface SQLiteContextProps {
         folder_id?: number;
         viewed_at?: string | Date;
     }) => void;
-    scanDocument: (id: number | null) => void;
+    scanDocument: (id: number | null, folderId: number | null) => void;
+    addDocument: (id: number | null, folderId: number | null, imglen: number) => void;
     fetchDocuments: () => void;
     setSortOption: (sortOption: string) => void;
     reOrderDocImages: (order: number, document_id: number, id: number) => void;
     generatePDF: (images: any, name: string) => Promise<any>;
     shareDocument: (pdfUri: string) => Promise<any>;
     saveDocument: (pdfUri: string, name: string) => Promise<any>;
+    updateDocName: (id: number, name: string) => void;
+    deleteDocImage: (id: number) => void;
+    deleteDocumentById: (id: number) => void;
+    deleteFolderById: (id: number) => void;
 }
 
 const SQLiteContext = createContext<SQLiteContextProps | null>(null);
@@ -38,9 +43,9 @@ const SQLiteContext = createContext<SQLiteContextProps | null>(null);
 const SQLiteProvider = ({ children }: { children: React.ReactNode }) => {
     const [folders, setFolders] = useState<Folder[]>([]);
     const [documents, setDocuments] = useState<Document[]>([]);
-    const [folderDocumnet, setFolderDocument] = useState<Document[]>();
+    const [folderDocumnet, setFolderDocument] = useState<Document[]>([]);
     const [img, setImg] = useState<any[]>([]);
-    const [sortOption, setSortOption] = useState<string>('date_newest');
+    const [sortOption, setSortOption] = useState<string>('viewed_at');
 
     const createFolder = async (folder: Folder) => {
         await insertFolder(folder);
@@ -148,7 +153,7 @@ const SQLiteProvider = ({ children }: { children: React.ReactNode }) => {
                 }
             }
         } catch (error) {
-            console.error('Error saving document:', error);
+            // console.error('Error saving document:', error);
             if (Platform.OS === 'android') {
                 ToastAndroid.show('Failed to save PDF!', ToastAndroid.LONG);
             }
@@ -157,26 +162,26 @@ const SQLiteProvider = ({ children }: { children: React.ReactNode }) => {
     };
 
     const fetchDocumentsByFolderId = async (id: number) => {
-        const query = 'SELECT * FROM documents WHERE folder_id = ?';
-
-        let doc: any = [];
+        const query = 'SELECT d.*, f.name AS folder_name FROM documents d LEFT JOIN folders f ON d.folder_id = f.id WHERE d.folder_id = ? ORDER BY d.created_at DESC';
 
         await db.transaction(tx => {
-            tx.executeSql(query, [id], (_tx, results) => {
-                for (let i = 0; i < results.rows.length; i++) {
-                    const row = results.rows.item(i);
-                    doc = {
-                        id: row.id,
-                        name: row.name,
-                        folder_id: row.folder_id,
-                        created_at: row.created_at,
-                        updated_at: row.updated_at,
-                        viewed_at: row.viewed_at,
-                    };
+            tx.executeSql(query, [id], (_, results) => {
+                const rows = results.rows;
+                for (let i = 0; i < rows.length; i++) {
+                    const row = rows.item(i);
+                    // console.log(row);
+                    tx.executeSql('SELECT * FROM images WHERE document_id = ? ORDER BY img_order', [row.id], (_tx, result) => {
+                        const imageRows = result.rows;
+                        let imgs: ImageProps[] = [];
+                        for (let j = 0; j < imageRows.length; j++) {
+                            const imageRow = imageRows.item(j);
+                            imgs.push(imageRow);
+                        }
+                        setFolderDocument((prev) => [...prev, { ...row, images: imgs }]);
+                    });
                 }
             });
         });
-        setFolderDocument(doc);
     };
 
     const fetchFolders = async () => {
@@ -195,7 +200,7 @@ const SQLiteProvider = ({ children }: { children: React.ReactNode }) => {
     };
 
     const fetchDocuments = async () => {
-        let orderByClause = 'ORDER BY viewed_at DESC';
+        let orderByClause = 'ORDER BY viewed_at DESC, created_at DESC';
         switch (sortOption) {
             case 'name_asc':
                 orderByClause = 'ORDER BY d.name ASC';
@@ -233,6 +238,7 @@ const SQLiteProvider = ({ children }: { children: React.ReactNode }) => {
                 setDocuments(items);
             });
         });
+        // console.log(documents);
     };
 
     const compressImage = async (path: string) => {
@@ -258,11 +264,11 @@ const SQLiteProvider = ({ children }: { children: React.ReactNode }) => {
         }
     };
 
-    const scanDocument = async (id: number | null) => {
+    const scanDocument = async (id: number | null, folderId: number | null) => {
         if (Platform.OS === 'android' && await PermissionsAndroid.request(
             PermissionsAndroid.PERMISSIONS.CAMERA
         ) !== PermissionsAndroid.RESULTS.GRANTED) {
-            Alert.alert('Error', 'User  must grant camera permissions to use document scanner.');
+            Alert.alert('Error', 'User must grant camera permissions to use document scanner.');
             return;
         }
 
@@ -271,15 +277,16 @@ const SQLiteProvider = ({ children }: { children: React.ReactNode }) => {
 
         // Create a new document
         const document = {
-            name: `ScanX_${new Date().getTime()}`,
-            folder_id: 1,
+            name: `ScanX_${new Date().toISOString()}`,
+            folder_id: folderId ? folderId : 1,
+            viewed_at: new Date().toISOString(),
         };
 
         // get back an array with scanned image file paths
         if (scannedImages && scannedImages.length > 0) {
             const documentId: any = id === null ? await insertDocument(document) : id;
             let order = 1;
-            for (const path of scannedImages) {
+            scannedImages.map(async (path) => {
                 try {
                     const compressedImage = await compressImage(path);
                     const destPath = `${RNFS.DocumentDirectoryPath}/ScanX_${new Date().getTime()}.jpg`;
@@ -293,6 +300,51 @@ const SQLiteProvider = ({ children }: { children: React.ReactNode }) => {
                     await RNFS.unlink(path);
                     order++;
                 } catch (error) {
+                    // console.error('Error processing image:', error);
+                    if (Platform.OS === 'android') {
+                        ToastAndroid.show('Error processing image', ToastAndroid.LONG);
+                    }
+                }
+            });
+            setImg(scannedImages);
+        }
+    };
+
+    const addDocument = async (id: number | null, folderId: number | null, imglen: number) => {
+        if (Platform.OS === 'android' && await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.CAMERA
+        ) !== PermissionsAndroid.RESULTS.GRANTED) {
+            Alert.alert('Error', 'User must grant camera permissions to use document scanner.');
+            return;
+        }
+
+        // start the document scanner
+        const { scannedImages } = await DocumentScanner.scanDocument();
+
+        // Create a new document
+        const document = {
+            name: `ScanX_${new Date().getTime()}`,
+            folder_id: folderId ? folderId : 1,
+        };
+
+        // get back an array with scanned image file paths
+        if (scannedImages && scannedImages.length > 0) {
+            const documentId: any = id === null ? await insertDocument(document) : id;
+            imglen++;
+            for (const path of scannedImages) {
+                try {
+                    const compressedImage = await compressImage(path);
+                    const destPath = `${RNFS.DocumentDirectoryPath}/ScanX_${new Date().getTime()}.jpg`;
+                    await RNFS.moveFile(compressedImage, destPath);
+                    const image: any = {
+                        path: destPath,
+                        document_id: documentId,
+                        img_order: imglen,
+                    };
+                    await insertImage(image);
+                    await RNFS.unlink(path);
+                    imglen++;
+                } catch (error) {
                     console.error('Error processing image:', error);
                 }
             }
@@ -300,10 +352,19 @@ const SQLiteProvider = ({ children }: { children: React.ReactNode }) => {
         }
     };
 
+    const updateDocName = (id: number, name: string) => {
+        let isDocument = documents.some((item) => item.id === id);
+        if (isDocument) {
+            let newDocument: Document | undefined = documents.find((item) => item.id === id);
+            newDocument!.name = name;
+            setDocuments([...documents]);
+        }
+    };
+
     const updateDocumentData = (data: {
         id: number;
         name?: string;
-        folder_id?: number;
+        folder_id?: number | string;
         viewed_at?: string | Date;
     }) => {
         updateDocument(data);
@@ -313,12 +374,54 @@ const SQLiteProvider = ({ children }: { children: React.ReactNode }) => {
         await reOrderDocumnetImages(order, document_id, id);
     };
 
+    const deleteDocImage = (id: number) => {
+        deleteImage(id);
+    };
+
+    // Get images by document id
+    const deleteImagesByDocumentId = async (document_id: number) => {
+        const query = `
+                SELECT *
+                FROM images
+                WHERE document_id = ?
+            `;
+
+
+        await db.transaction(tx => {
+            tx.executeSql(query, [document_id], (_tx, results) => {
+                const imgs: ImageProps[] = [];
+                for (let i = 0; i < results.rows.length; i++) {
+                    const row = results.rows.item(i);
+                    imgs.push(row);
+                }
+                imgs.map(async (item) => await RNFS.unlink(item?.path));
+            });
+        });
+    };
+
+    const deleteDocumentById = async (id: number) => {
+        const newDocument = documents.filter((item) => item.id !== id);
+        await deleteImagesByDocumentId(id);
+        deleteDocument(id);
+        setDocuments(newDocument);
+    };
+
+    const deleteFolderById = async (id: number) => {
+        await fetchDocumentsByFolderId(id);
+        folderDocumnet.map(async (document) => (await deleteDocumentById(document.id)));
+        const newFoleder = folders.filter((item) => item.id !== id);
+        await deleteFolder(id);
+        setFolders(newFoleder);
+    };
+
+
     useEffect(() => {
         fetchDocuments();
         fetchFolders();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [img, sortOption]);
 
+    // console.log(folderDocumnet);
 
     const value: SQLiteContextProps = {
         folders,
@@ -335,6 +438,11 @@ const SQLiteProvider = ({ children }: { children: React.ReactNode }) => {
         generatePDF,
         shareDocument,
         saveDocument,
+        updateDocName,
+        deleteDocImage,
+        deleteDocumentById,
+        deleteFolderById,
+        addDocument,
     };
 
     return (
